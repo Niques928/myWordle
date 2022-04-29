75117eb42f901b36794aacd67699298c89091ee3
Chinique Joseph <chiniquejoseph@MacBook-Pro-3.fios-router.home>
2022-04-29 19:36:08 -0400

    Created components heiarchy

---
+++ src/components/Keyboard.js
@@ -0,0 +1,11 @@
+
+function Keyboard() {
+
+    return (
+        <div>
+            <h1> Keyboard</h1>
+        </div>
+    )
+}
+
+export default Keyboard
---
+++ src/components/WordleGame.js
@@ -0,0 +1,15 @@
+import Board from './Board'
+import Keyboard from './Keyboard'
+
+function WordleGame() {
+
+    return (
+        <div>
+            <h1> Game Container </h1>
+            <Board />
+            <Keyboard />
+        </div>
+    )
+}
+
+export default WordleGame
