f85181a4dc8394eb4334e6505f7aafe25dc41965
Chinique Joseph <chiniquejoseph@MacBook-Pro-3.fios-router.home>
2022-04-29 20:09:25 -0400

    Styles folder added and Visible Keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,9 +1,27 @@
+import '../styles/Keyboard.css'
 
 function Keyboard() {
 
+    let Keys1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']
+    let Keys2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
+    let Keys3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
     return (
-        <div>
-            <h1> Keyboard</h1>
+        <div className="keyboard">
+            <div className ="row">
+               {Keys1.map((key)=>{
+                return <div> {key}</div>
+            })} </div>
+            <br></br>
+            <div className ="row">
+            {Keys2.map((key)=>{
+            return <div> {key}</div>
+        })} </div>
+        <br></br>
+        <div className ="row">
+            {Keys3.map((key)=>{
+            return <div> {key}</div>
+        })} </div>
         </div>
     )
 }
